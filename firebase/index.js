81baf05f7a17624442c2835a7940343bf0fcc1be
Cody Huynh <tunghuynh1996@gmail.com>
2023-09-04 03:08:53 +0700

    Fix: remove plaintext json file of firebase cert

--- firebase/index.js
+++ firebase/index.js
@@ -1,9 +1,17 @@
 const admin = require('firebase-admin');
+const cert = require('./config.json');
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_CERT);
+const firebaseCert = JSON.stringify(cert);
+console.info(firebaseCert);
+
+const serviceAccount = firebaseCert
+	.replace('{{private_key_id}}', process.env.FIREBASE_CERT_KEY_ID)
+	.replace('{{private_key}}', process.env.FIREBASE_CERT_KEY);
+
+console.info(serviceAccount);
 
 admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount),
+	credential: admin.credential.cert(JSON.parse(serviceAccount)),
 });
 
 module.exports = admin;
